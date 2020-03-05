e18a229008834fd4172b28c3732212e7ef1d710d
Patton Parham <patton.parham@verizon.net>
2020-03-05 00:54:51 -0600

    set up router to the best of my ability

---
+++ Develop/routes/apiroute.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const Workouts = require("../models/workouts.js");
+
+router.post("/api/workouts", ({body}, res) =>{
+    Workouts.create(body)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/exercises", ({body}, res) =>{
+    Workouts.insertMany(body)
+    .then(dbWorkouts => {
+        res.json(dbWorkouts);
+    })
+    .catch(err =>{
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workouts.find({})
+    .sort({
+        // idk what to do here yet and am getting tired
+    })
+    .then(dbWorkouts =>{
+        res.json(dbWorkouts);
+    })
+    .catch(err =>{
+        res.status(400).json(err);
+    });
+});
+
+module.exports = router;
